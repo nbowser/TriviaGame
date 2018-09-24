c3310d803158fc83c898464975aceebd6c94063a
Neal Bowser <rh1thmmomk@gmail.com>
2018-09-23 23:19:07 -0400

    major repairs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TriviaGame
+
+This trivia game is using html5, CSS3, Bootstrap, and jQuery technoligies to bring enjoyment to the trivia market.
+
+To play:
+
+* Click the start button
+* Answer the questions bly clicking the button of your choice (one choice per question)
+* See how much you know about the real world!
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,138 +1,33 @@
 /* first need to pseudo code the steps needing to take*/
 
 // need some variables
-
-
-
-// some css style to make this look good
-
-// Variables
-var counter = 30;
-
-// Click commands
-$(document).on('click', '#start-over', function(e) {
-    game.rest();
-});
-
-$(document).on('click', '.answer-button', function(e) {
-    game.clicked(e);
-});
-
-$(document).on('click', '#start', function(e) {
-    $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number"></span> Seconds</h2');
-    game.loadQuestion();
-});
-
-// Question Array/object
-var questions = [ {
+$(document).ready(function(){
+  
+    // event listeners
+    $("#remaining-time").hide();
+    $("#start").on("click", trivia.startGame);
+    $(document).on("click" , ".option", trivia.guessChecker);
     
-    question: "When Morpheus offered Neo a choice to know what the 'Matrix' is, what pill did he take from Morpheus' hand?",
-    answers: ["Red", "Blue", "Green", "Yellow"],
-    correctAnswer: "Red",
-    images: ""
-}, {
-    question: "Which Agent was more fixated on capturing Morpheus than the rest?",
-    answers: ["Agent Brown", "Agent Orange", "Agent Smith", "Agent Jones"],
-    correctAnswer: "Agent Smith",
-    images: ""
-}, {
-    question: "What pod born crew member was considered to be the 'Judas' of the bunch?",
-    answers: ["Trinity", "Switch", "Apoc", "Mouse", "Cypher"],
-    correctAnswer: "Cypher",
-    images: ""
-}, {
-    question: "What was the name of the operator on the ship?",
-    answers: ["Dozer", "Blade", "Tank", "Gunner"],
-    correctAnswer: "Tank",
-    images: ""
-}, {    
-    question: "What is the underlining issue about the path people are on either in the 'Real World' or the Matrix?",
-    answers: ["Choice", "Control", "Destiny", "Reality"],
-    correctAnswer: "Choice",
-    images: ""
-}, {
-    question: "What is the name of the hovercraft Morpheus captains?",
-    answers: ["Shadrach", "Bonaparte", "Belshazzar", "Nebuchadnezzar", "Ramses"],
-    correctAnswer: "Nebuchadnezzar",
-    images: ""
-}, {
-    question: "What was the 'Oracle' making when Neo came to visit her?",
-    answers: ["Apple Pie", "Candy", "Cookies", "Baklava", "Bread"],
-    correctAnswer: "Cookies",
-    images: ""
-}, {  
-    question: "What is the food Dozer calls the 'breakfast of champions'?",
-    answers: ["Goop", "Tasty Wheat", "Chicken", "Tuna Fish"],
-    correctAnswer: "Goop",
-    images: ""
-}, {
-    question: "What does Agent Smith find most repolsive about the Matrix?",
-    answers: ["It never changes", "People being too oblivious", "Having to spend time with humans", "The smell"],
-    correctAnswer: "The smell",
-    images: ""
-}, {
-    question: "What song is playing in Neo's headphones right before Trinity contacts him on his computer?",
-    answers: ["Wake Up by Rage Against the Machine", "Dissolved Girl by Massive Attack", "Mindfields by The Prodigy", "Sleeping Awake by P.O.D."],
-    correctAnswer: "Dissolved Girl by Massive Attack",
-    images: ""
-}],
-
-
-// Functions
-var game = guestions[Math.floor(Math.random() * questions.length)];
-
-
-
-
-// Need a way to insert questions and choises into game and randomize them
-
-
-
-for (var i = 0; i < questions.length; i++) {
-    for (var j = 0; j < answers.length; j++) {
-
-    }
-}
-
-
-// Need a way validate/log player choices
-if (correctAnswer === true) {
-    //add 1 point to correctAnswer score;
-}
-
-else  {
-    //add 1 point to wrongAnswer score; 
-};
-
-// Display questions and be able to select answer
-
-
-
-
-// Need a way to count down timer for game 
-
-// refer to timeout.html exercise
-$('#start').on("click", (function() {
-    counter = setTimeout(function() {
-
-    }, 30000);
-});
-
-
-
-// Need a method to keep score of right/wrong answers
-var correctAnswers = 0;
-var  wrongAnswers= 0;
-
-
-//Player wins/losses
+  })
+  
+  var trivia = {
+    // trivia properties
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 20,
+    timerOn: false,
+    timerId : "",
+    
+    
+    //Player wins/losses
 
 /*
-if (correctAnswer >= 6) {
+if (correct >= 6) {
     alert("Welcome to the real world!");
     images: pop up from pod
     end game
-
 
 }
 
@@ -142,11 +37,183 @@ else if (counter = 0 && correctAnswers < 6 || wrongAnswers = 5) {
     end game
 }
 */
+    // questions options and answers data
+    questions: {
+      q1: "When Morpheus offered Neo a choice to know what the 'Matrix' is, what pill did he take from Morpheus' hand?",
+      q2: "Which Agent was more fixated on capturing Morpheus than the rest?",
+      q3: "What pod born crew member was considered to be the 'Judas' of the bunch?",
+      q4: "What was the name of the operator on the ship?",
+      q5: "What is the underlining issue about the path people are on either in the 'Real World' or the Matrix?",
+      q6: "What is the name of the hovercraft Morpheus captains?",
+      q7: "What was the Oracle making when Neo came to visit her?",
+      q8: "What is the food Dozer calls the 'breakfast of champions'?",
+      q9: "What does Agent Smith find most repolsive about the Matrix?",
+      q10: "What song is playing in Neo's headphones right before Trinity contacts him on his computer?"
+    },
+    options: {
+      q1: ["Red", "Blue", "Green", "Yellow"],
+      q2: ["Agent Brown", "Agent Orange", "Agent Smith", "Agent Jones"],
+      q3: ["Switch", "Apoc", "Mouse", "Cypher"],
+      q4: ["Dozer", "Razor", "Blade", "Tank"],
+      q5: ["Choice", "Control", "Destiny", "Reality"],
+      q6: ["Shadrach", "Bonaparte", "Belshazzar", "Nebuchadnezzar", "Ramses"],
+      q7: ["Apple Pie", "Candy", "Cookies", "Baklava", "Bread"],
+      q8: ["Goop", "Tasty Wheat", "Chicken", "Tuna Fish"],
+      q9: ["It never changes", "People being too oblivious", "Having to spend time with humans", "The smell"],
+      q10: ["Wake Up by Rage Against the Machine", "Dissolved Girl by Massive Attack", "Mindfields by The Prodigy", "Sleeping Awake by P.O.D."]
+    },
+    answers: {
+      q1: "Red",
+      q2: "Agent Smith",
+      q3: "Cypher",
+      q4: "Tank",
+      q5: "Choice",
+      q6: "Nebuchadnezzar",
+      q7: "Cookies",
+      q8: "Goop",
+      q9: "The smell",
+      q10: "Dissolved Girl by Massive Attack"
+    },
+    // trivia methods
+    // method to initialize game
+    startGame: function(){
+      // restarting game results
+      trivia.currentSet = 0;
+      trivia.correct = 0;
+      trivia.incorrect = 0;
+      trivia.unanswered = 0;
+      clearInterval(trivia.timerId);
+      
+      // show game section
+      $("#game").show();
+      
+      //  empty last results
+      $("#results").html("");
+      
+      // show timer
+      $("#timer").text(trivia.timer);
+      
+      // remove start button
+      $("#start").hide();
+  
+      $("#remaining-time").show();
+      
+      // ask first question
+      trivia.nextQuestion();
+      
+    },
+    // method to loop through and display questions and options 
+    nextQuestion : function(){
+      
+      // set timer-seconds each question
+      trivia.timer = 12;
+       $("#timer").removeClass("last-seconds");
+      $("#timer").text(trivia.timer);
+      
+      // to prevent timer speed up
+      if(!trivia.timerOn){
+        trivia.timerId = setInterval(trivia.timerRunning, 1000);
+      }
+      
+      // gets all the questions then indexes the current questions
+      var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+      $("#question").text(questionContent);
+      
+      // an array of all the user options for the current question
+      var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+      
+      // creates all the trivia guess options in the html
+      $.each(questionOptions, function(index, key){
+        $("#options").append($('<button class="option btn btn-info btn-lg">' + key + '</button>'));
+      })
+      
+    },
+    // method to decrement counter and count unanswered if timer runs out
+    timerRunning : function(){
+      // if timer still has time left and there are still questions left to ask
+      if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+        $("#timer").text(trivia.timer);
+        trivia.timer--;
+          if(trivia.timer === 4){
+            $("#timer").addClass("last-seconds");
+          }
+      }
+      // the time has run out and increment unanswered, run result
+      else if(trivia.timer === -1){
+        trivia.unanswered++;
+        trivia.result = false;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 3000);
+        $("#results").html("<h3>The body cannot live without the mind!" + "<br>" + Object.values(trivia.answers)[trivia.currentSet] +"</h3>");
+      }
+      // if all the questions have been shown end the game, show results
+      else if(trivia.currentSet === Object.keys(trivia.questions).length){
+        
+        // adds results of game (correct, incorrect, unanswered) to the page
+        $("#results")
+          .html("<h3>Welcome to the desert of the real!</h3>"+
+          "<p>Correct: " + trivia.correct + "</p>" +
+          "<p>Incorrect: " + trivia.incorrect + "</p>" +
+          "<p>Unaswered: " + trivia.unanswered +"</p>" +
+          "<p>Play again?</p>");
+        
+        // hide game sction
+        $("#game").hide();
+        
+        // show start button to begin a new game
+        $("#start").show();
+      }
+      
+    },
+    // method to evaluate the option clicked
+    guessChecker : function() {
+      
+      // timer ID for gameResult setTimeout
+      var resultId;
+      
+      // the answer to the current question being asked
+      var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+      
+      // if the text of the option picked matches the answer of the current question, increment correct
+      if($(this).text() === currentAnswer){
+        // turn button green for correct
+        $(this).addClass("btn-success").removeClass("btn-info");
+        
+        trivia.correct++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 3500);
+        $("#results").html("<h3>Good your beginning to believe!</h3>");
+      }
+      // else the user picked the wrong option, increment incorrect
+      else{
+        // turn button clicked red for incorrect
+        $(this).addClass("btn-danger").removeClass("btn-info");
+        
+        trivia.incorrect++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 5000);
+        $("#results").html("<h3>The answer is out there, it's looking for you, and it will find you if you want it to!" + "<br>" + currentAnswer +"</h3>");
+      }
+      
+    },
+    // method to remove previous question results and options
+    guessResult : function(){
+      
+      // increment to next question set
+      trivia.currentSet++;
+      
+      // remove the options and results
+      $(".option").remove();
+      $("#results h3").remove();
+      
+      // begin next question
+      trivia.nextQuestion();
+       
+    }
+  
+  }
 
-
-
-
-// A reset Answer counter, clock, and to randomize questions.
+  // A reset Answer counter, clock, and to randomize questions.
 
 /* $(":reset") {
     game
@@ -154,10 +221,8 @@ else if (counter = 0 && correctAnswers < 6 || wrongAnswers = 5) {
     correctAnswers = 0;
     wrongAnswers = 0
 }
-
 -----------------------------------------------
 OR
-
 function updatequiz-area() {
     my
 }
